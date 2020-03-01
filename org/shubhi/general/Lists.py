c7929317afaac22c257d98349e5c0704761471b4
shubhdashambhavi <shubhdashambhavi14@gmail.com>
2020-03-01 21:47:55 +0530

    Lists

--- org/shubhi/general/Lists.py
+++ org/shubhi/general/Lists.py
@@ -6,7 +6,7 @@ print(my_list)
 print(my_list_2)
 
 #Accessing elements of the list
-
+'''
 print(my_list[:])  # Accessing all elements of the list
 print(my_list[3])  # Accessing the 3rd element of the list
 print(my_list[0:4])  # Accessing the elements from 1 to 3, i.e, index 0 to 3 and leaving index 4
@@ -30,3 +30,36 @@ print(my_list + ['just for example'])  # Concatenation
 print(my_list * 2)  # Multiply
 print(my_list)
 
+
+#Deleting elements from the list
+
+my_list_2.extend([555,12,'example'])  # extending list by adding elements one by one
+print(my_list_2)
+del my_list_2[5]  # deleting using delete keyboard
+print(my_list_2)
+
+my_list_2.remove('example')  # remove the passed element if present else error
+print(my_list_2)
+
+a= my_list_2.pop(5)  # deleted element can be retrieved
+print('Popped Element: ', a, 'List remaining: ', my_list_2)
+
+my_list_2.clear()
+print(my_list_2)
+
+'''
+
+#other Functions
+new=[5,4,12,45,3]
+print(len(my_list))  # find length of the list
+print(my_list.index('edureka'))  # finds index of the element
+print(my_list.count('edureka'))  # finds count of times repeated in the list
+print(sorted(new))  # sorts element in the list
+print(new)
+new.sort(reverse=True)  # sort list in descending order
+print(new)
+new.reverse()  # reverse list elements
+print(new)
+
+new_list = my_list.copy()  #copies elements of the list in new list
+print(new_list)

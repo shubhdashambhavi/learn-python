4cac479ec71340894d6c432e744546c24dc7a897
shubhdashambhavi <shubhdashambhavi14@gmail.com>
2020-03-02 00:27:02 +0530

    Sets

--- org/shubhi/general/Sets.py
+++ org/shubhi/general/Sets.py
@@ -21,3 +21,14 @@ print(my_set.difference(my_set_2), '---', my_set - my_set_2)
 print(my_set.symmetric_difference(my_set_2), '-----', my_set^my_set_2)
 my_set.clear()  # empty the set
 print(my_set)
+
+#Operators with sets
+superset ={1,2,3,4,5,6,7,8,9,10}
+s1 = {1,2,3,4}
+s2 = {3,4,5,6}
+print(s1 == s2) # If symmetric
+print(s1 != s2) # if not symmetric
+print(s1 <= superset) # If s1 is a subset of superset
+print(s1 < superset) # if s1 is a proper subset of superset
+print(s1 >= s2) # is s2 is a subset of s1
+print(s1 > s2) # if s2 is a proper subset of s1
